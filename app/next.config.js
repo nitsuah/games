77b713410b1ee9e57d30786a3bfedbee91fc514a
nitsuahlabs@gmail.com <nitsuahlabs@gmail.com>
2025-10-28 04:24:04 -0400

    refactor: remove experimental CSS optimization from Next.js configuration

--- app/next.config.js
+++ app/next.config.js
@@ -4,10 +4,6 @@ const nextConfig = {
   compiler: {
     styledComponents: true, // Enables styled-components support
   },
-  // Enable experimental optimizations
-  experimental: {
-    optimizeCss: true, // Optimize CSS output
-  },
   // Optimize bundle to reduce unused JavaScript
   webpack: (config, { dev, isServer }) => {
     if (!dev && !isServer) {
