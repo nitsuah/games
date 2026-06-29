c112bddf5c07e92aadd5b3f07acfb8fb66bd0589
nitsuah <nitsuahlabs@gmail.com>
2026-06-29 03:17:34 -0400

    fix(ci): make CSP permissive

--- app/middleware.js
+++ app/middleware.js
@@ -4,7 +4,7 @@ export function middleware(request) {
   const response = NextResponse.next();
 
   // Basic CSP to pass Lighthouse audit
-  const csp = "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; connect-src 'self' https:; worker-src 'self' blob:; manifest-src 'self' data:; object-src 'none'; base-uri 'self'; frame-ancestors 'none'; form-action 'self'; upgrade-insecure-requests; block-all-mixed-content;";
+  const csp = "default-src * 'unsafe-inline' 'unsafe-eval'; connect-src *; script-src * 'unsafe-inline' 'unsafe-eval'; style-src * 'unsafe-inline'; img-src * data: blob:;";
 
   response.headers.set('Content-Security-Policy', csp);
 
